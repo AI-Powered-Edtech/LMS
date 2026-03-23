63bb21b7b64e8c7f1f2643d0a790b94b00a748b1
IbrohimMid <m.ibrohim@proton.me>
2026-03-24 00:11:49 +0700

    chore: update gitignore and untrack local temporary files

--- get_table_def.js
+++
@@ -1,3 +0,0 @@
-const { execSync } = require('child_process');
-const output = execSync('psql -U postgres -d postgres -c "\\d public.quiz_attempts"', { encoding: 'utf8', env: {...process.env, PGPASSWORD: 'postgres'} });
-console.log(output);
